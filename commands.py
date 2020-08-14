ca2291e58c43b8cbbfd2a6c5a4fe1bd25c8488c1
Maksymilian Kalek <maks.kalek@gmail.com>
2020-08-14 11:19:54 +0200

    changes

--- commands.py
+++ commands.py
@@ -1,10 +1,12 @@
 import click
 from flask.cli import with_appcontext
 
-from app import  db
+from app import  app, db
 from app.models import User, Cat
 
 @click.command(name="create_tables")
 @with_appcontext
 def create_tables():
     db.create_all()
+
+create_tables()
