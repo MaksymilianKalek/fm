f34bba0da864c80605a907d0c5a8652d43dbf708
Maksymilian Kalek <maks.kalek@gmail.com>
2020-08-14 11:35:27 +0200

    changes

--- addToDB.py
+++ addToDB.py
@@ -1,7 +1,8 @@
 from app import db
 from app.models import User, Cat
+import sys
 
-u = User(username="fabrykaAdmin")
-u.set_password("kitku987")
+u = User(username=str(sys.argv[0]))
+u.set_password(str(sys.argv[1]))
 db.session.add(u)
 db.session.commit()
