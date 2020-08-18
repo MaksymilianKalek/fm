557473eeaabfdbe01f03b44a860bb4a28aa0ab66
Maksymilian Kalek <maks.kalek@gmail.com>
2020-08-18 11:20:43 +0200

    d

--- helpers.py
+++ helpers.py
@@ -1,11 +1,11 @@
 import boto3, botocore
 from config import Config
-import aws
+from aws import key, secret
 
 s3 = boto3.client(
    "s3",
-   aws_access_key_id=os.environ.get("S3_ACCESS_KEY") or aws.key,
-   aws_secret_access_key=os.environ.get("S3_SECRET_ACCESS_KEY") or aws.secret
+   aws_access_key_id=os.environ.get("S3_ACCESS_KEY") or key,
+   aws_secret_access_key=os.environ.get("S3_SECRET_ACCESS_KEY") or secret
 )
 
 def upload_file_to_s3(file, bucket_name, acl="public-read"):
